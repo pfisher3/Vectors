887a90ea2b809138bf130bc2d5a52df488a40e49
Phillip Fisher <pfisher@hugeinc.com>
2015-06-09 10:48:40 -0400

    initial commit

--- src/CinderProjectApp.cpp
+++ src/CinderProjectApp.cpp
@@ -1,5 +1,6 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include "cinder/Camera.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -11,24 +12,82 @@ class CinderProjectApp : public AppNative {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+    void resize();
+    
+    Matrix44f mCubeRotation;
+    CameraPersp mCam;
+    
+//    Adding Light direction and position
+    Vec3f   mLightDirection;
+    ColorAf mColor;
+    
 };
 
 void CinderProjectApp::setup()
 {
+    Vec3f eyePoint(200.0f, 300.0f, -400.0f);
+    Vec3f target = Vec3f::zero();
+    mCam.lookAt(eyePoint, target);
+    mCubeRotation.setToIdentity();
+    
+//    Calling the Light direction
+    mLightDirection = Vec3f(100, 100, -400.0f);
 }
 
 void CinderProjectApp::mouseDown( MouseEvent event )
 {
 }
+void CinderProjectApp::resize(){
+    mCam.setPerspective(60, getWindowAspectRatio(), 1, 1000);
+    gl::setMatrices(mCam);
+}
 
 void CinderProjectApp::update()
 {
+    mCubeRotation.rotate(Vec3f( 1, 1, 1), toRadians(0.2f));
 }
 
 void CinderProjectApp::draw()
 {
+    
+    gl::enableDepthRead();
+    gl::enableDepthWrite();
+    
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
+    
+    Vec2i pos = getMousePos();
+    mColor = ColorAf(1.0f, lmap<float>(pos.x, 0.0f, (float)getWindowWidth(), 0.0f, 1.0f),  0.0f);
+    
+    gl::color(ColorAf::white());
+    
+    glLoadIdentity();
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    GLfloat lightPosition[] = {
+        -mLightDirection.x,
+        -mLightDirection.y,
+        -mLightDirection.z,
+        0
+    };
+    
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mColor);
+    
+    gl::setMatrices(mCam);
+    
+    
+    
+    Vec3f size(200.0f, 200.0f, 200.0f);
+    Vec3f center(getWindowCenter(), 0);
+    
+//    gl::drawSolidRect(getWindowBounds());
+    glPushMatrix();
+        gl::multModelView( mCubeRotation);
+        gl::drawCube(Vec3f::zero(), size);
+    glPopMatrix();
+    
+  
 }
 
 CINDER_APP_NATIVE( CinderProjectApp, RendererGl )
---
+++ xcode/build/CinderProject.build/Debug/CinderProject.build/Objects-normal/i386/CinderProjectApp.d
@@ -0,0 +1,3 @@
+dependencies: /Users/pfisher/CinderProject/src/CinderProjectApp.cpp \
+  ../../../../Applications/cinder_0.8.6_mac/include/cinder/app/AppNative.h \
+  ../../../../Applications/cinder_0.8.6_mac/include/cinder/Camera.h
